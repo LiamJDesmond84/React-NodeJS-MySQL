bc1e06ff14a700a2c3ed95e3401e3d6a1e929d69
Liam Desmond <liamjdesmond84@gmail.com>
2022-03-26 19:18:31 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 8000;
+
+const db = require('./models');
+
+app.use(express.json())
+// app.use(cors());
+
+
+const postRouter = require('./routes/Posts');
+
+app.use("/posts", postRouter);
+
+
+
+
+
+db.sequelize.sync().then(() => {
+
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    });
+
+})
---
+++ server/routes/Posts.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const { Posts } = require("../models");
+
+//# Get All
+router.get("/", (req, response) => {
+
+    Posts.findAll()
+    .then(posts => response.json(posts))
+    .catch((err)=>{console.log(err);response.status(400).json(err)})
+});
+
+//# Get One
+router.get("/:id", (req, response) => {
+
+    Posts.findByPk(req.params.id)
+    .then(post => response.json(post))
+    .catch((err)=>{console.log(err);response.status(400).json(err)})
+    
+})
+//# Create One
+router.post("/", (req, response) => {
+
+    Posts.create(req.body)
+    .then(post => response.json(post))
+    .catch((err)=>{console.log(err);response.status(400).json(err)})
+
+})
+
+//# Update One
+router.put("/:id", (req, response) => {
+
+    Posts.update(req.body, { where :{id: req.params.id}}, { multi: true })
+    .then(updatedPost => response.json(updatedPost))
+    .catch((err)=>{console.log(err);response.status(400).json(err)})
+    
+})
+
+//# Delete One
+router.delete("/:id", (req, response) => {
+
+    Posts.destroy({ where :{id: req.params.id}}, {truncate: true})
+    .then(deleteConfirmation => response.json(deleteConfirmation))
+    .catch((err)=>{console.log(err);response.status(400).json(err)})
+    
+})
+
+
+module.exports = router;
